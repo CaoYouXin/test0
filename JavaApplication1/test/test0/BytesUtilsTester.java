0dfb2603a509bb4ffe7cf7c0b2bdc83c3b2dfe6b
曹力升 <c_lisheng@163.com>
2014-09-11 11:10:54 +0800

    我想说IntelliJ很强大，但就是用不惯。。。

---
+++ JavaApplication1/test/test0/BytesUtilsTester.java
@@ -0,0 +1,12 @@
+package test0;
+
+/**
+ * Created by caolisheng on 2014/9/11 011.
+ */
+public class BytesUtilsTester {
+
+    public static void main(String args[]) {
+        System.out.println("adsf");
+    }
+
+}
