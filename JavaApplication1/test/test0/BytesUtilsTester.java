13ecd8625b99a229ae7b4682ba32f148de27b86e
曹力升 <c_lisheng@163.com>
2014-09-11 11:19:47 +0800

    改为JUnit4实例

--- JavaApplication1/test/test0/BytesUtilsTester.java
+++ JavaApplication1/test/test0/BytesUtilsTester.java
@@ -1,12 +1,36 @@
 package test0;
 
+import static org.junit.Assert.*;
+
+import java.util.Arrays;
+
+import org.junit.Test;
+
+import util.BytesUtils;
+import util.Debugger;
+import util.BytesUtils.Offset;
+
 /**
  * Created by caolisheng on 2014/9/11 011.
  */
 public class BytesUtilsTester {
 
-    public static void main(String args[]) {
-        System.out.println("adsf");
-    }
+	@Test
+	public void testShort() {
+		Debugger.timeCounting(() -> {
+			byte[] buf = null;
+			for (int s = Short.MIN_VALUE; s <= Short.MAX_VALUE; s++) {
+//		for (int s = -10; s <= 10; s++) {
+				buf = BytesUtils.encodeShort((short) s);
+				short bs = BytesUtils.decodeShort(buf, new Offset());
+				if (bs != s) {
+					System.err.println(String.format("Wrong with %d while bs = %d, and buf = %s", s, bs
+							, Arrays.toString(buf)));
+				}
+			}
+		}, (cost) -> {
+			System.out.println("Time = " + cost / Math.pow(10, 9) + "s");
+		});
+	}
 
 }
